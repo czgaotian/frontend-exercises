474e9a332bc8a69cde111d745e48e31d8e56a11f
petrarch <811460602@qq.com>
2022-06-10 02:10:49 +0800

    依赖收集

--- packages/reactivity/src/baseHandler.ts
+++ packages/reactivity/src/baseHandler.ts
@@ -1,4 +1,4 @@
-import { activeEffect } from './effect';
+import { activeEffect, track, trigger } from "./effect";
 
 export const enum ReactiveFlags {
   IS_REACTIVE = "__v_isReactive",
@@ -9,9 +9,17 @@ export const mutableHandlers = {
     if (key === ReactiveFlags.IS_REACTIVE) {
       return true;
     }
+
+    track(target, "get", key);
+
     return Reflect.get(target, key, receiver);
   },
   set(target, key, value, receiver) {
-    return Reflect.set(target, key, value, receiver);
-  }
-}
+    let oldValue = target[key];
+    let result = Reflect.set(target, key, value, receiver);
+    if (oldValue !== value) {
+      trigger(target, "set", key, value, oldValue);
+    }
+    return result;
+  },
+};
--- packages/reactivity/src/effect.ts
+++ packages/reactivity/src/effect.ts
@@ -1,10 +1,11 @@
 export let activeEffect = undefined;
 
-class createReactiveEffect {
+class ReactiveEffect {
+  public parent = null;
+  // 记录依赖其的属性
+  public deps = [];
   public active = true;
-  constructor(public fn){
-
-  }
+  constructor(public fn) {}
 
   run() {
     if (!this.active) {
@@ -13,16 +14,49 @@ class createReactiveEffect {
 
     // 依赖收集
     try {
+      this.parent = activeEffect;
       activeEffect = this;
       this.fn();
     } finally {
-      activeEffect = undefined;
+      activeEffect = this.parent;
+      this.parent = null;
     }
   }
 }
 
 export const effect = (fn) => {
-  const _effect = new createReactiveEffect(fn);
+  const _effect = new ReactiveEffect(fn);
 
-  _effect.run()
-}
+  _effect.run();
+};
+
+const targetMap = new WeakMap();
+export const track = (target, type, key) => {
+  if (!activeEffect) return;
+  let depsMap = targetMap.get(target);
+  if (!depsMap) {
+    targetMap.set(target, (depsMap = new Map()));
+  }
+  let dep = depsMap.get(key);
+  if (!dep) {
+    depsMap.set(key, (dep = new Set()));
+  }
+  let shouldTrack = !dep.has(activeEffect);
+  if (shouldTrack) {
+    dep.add(activeEffect);
+    // 存放属性对应的Set<ReactiveEffect>
+    activeEffect.deps.push(dep);
+  }
+};
+
+export const trigger = (target, type, key, newValue, oldValue) => {
+  const depsMap = targetMap.get(target);
+  if (!depsMap) return;
+  const effects = depsMap.get(key);
+  effects &&
+    effects.forEach((effect) => {
+      if (effect.active) {
+        if (effect !== activeEffect) effect.run();
+      }
+    });
+};
