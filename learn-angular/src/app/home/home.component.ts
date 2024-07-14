0f350459ed3af82b258afcc4c7faf3f742fce1fa
mean no harm <811460602@qq.com>
2024-07-15 01:09:45 +0800

    13-search

--- learn-angular/src/app/home/home.component.ts
+++ learn-angular/src/app/home/home.component.ts
@@ -14,7 +14,20 @@ import { HousingLocation } from '../housinglocation';
 export class HomeComponent {
   housingService: HousingService = inject(HousingService);
   housingLocationList: HousingLocation[];
+  filteredLocationList: HousingLocation[] = [];
+
   constructor() {
     this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.filteredLocationList = this.housingLocationList;
+  }
+
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+    this.filteredLocationList = this.housingLocationList.filter((housingLocation) =>
+      housingLocation?.city.toLowerCase().includes(text.toLowerCase()),
+    );
   }
 }
