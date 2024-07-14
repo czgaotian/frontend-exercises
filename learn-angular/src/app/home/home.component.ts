f3d0e2bab17335a99c7ba1a28b0bea84384e064f
mean no harm <811460602@qq.com>
2024-07-15 00:43:14 +0800

    finished 11-details-page

--- learn-angular/src/app/home/home.component.ts
+++ learn-angular/src/app/home/home.component.ts
@@ -1,27 +1,20 @@
-import { Component } from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {HousingLocationComponent} from '../housing-location/housing-location.component';
-import {HousingLocation} from '../housinglocation';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { HousingService } from '../housing.service';
+import { HousingLocation } from '../housinglocation';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [CommonModule, HousingLocationComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-  readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
-
-  housingLocation: HousingLocation = {
-    id: 9999,
-    name: 'Test Home',
-    city: 'Test city',
-    state: 'ST',
-    photo: `${this.baseUrl}/example-house.jpg`,
-    availableUnits: 99,
-    wifi: true,
-    laundry: false,
-  };
-
+  housingService: HousingService = inject(HousingService);
+  housingLocationList: HousingLocation[];
+  constructor() {
+    this.housingLocationList = this.housingService.getAllHousingLocations();
+  }
 }
