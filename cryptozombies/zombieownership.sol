d3744be781fad82b5a437e5a2d2f271a98e3ac3c
czgaotian <811460602@qq.com>
2024-12-08 12:23:37 +0100

    lesson 5 chapter 2

---
+++ cryptozombies/zombieownership.sol
@@ -0,0 +1,10 @@
+pragma solidity >=0.5.0 <0.6.0;
+
+import "./zombieattack.sol";
+// Import file here
+import "./erc721.sol";
+
+// Declare ERC721 inheritance here
+contract ZombieOwnership is ZombieAttack, ERC721 {
+
+}
