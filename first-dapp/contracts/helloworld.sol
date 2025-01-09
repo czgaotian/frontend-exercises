0a3d6c72ef72bb111adb96c8ba7c190dbce47f96
czgaotian <811460602@qq.com>
2025-01-09 01:11:05 +0100

    1 hello world

---
+++ first-dapp/contracts/helloworld.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract helloword {
+    function hello() public pure returns (string memory) {
+        return "hello world";
+    }
+}
