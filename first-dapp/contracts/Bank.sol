42ae44a166772885238611d16fb75f09319462f7
czgaotian <811460602@qq.com>
2025-01-09 16:48:32 +0100

    transfer and withdraw

--- first-dapp/contracts/Bank.sol
+++ first-dapp/contracts/Bank.sol
@@ -2,9 +2,10 @@
 pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/interfaces/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract Bank {
-    mapping(address => uint) public deposited;
+    mapping(address => uint256) public deposited;
 
     address public immutable token;
 
@@ -12,13 +13,39 @@ contract Bank {
         token = _token;
     }
 
-    function myBalance() public view  returns(uint balance) {
-        balance = deposited[msg.sender] / (10 ** 18);
+    modifier requireBalance(uint256 amount) {
+        amount = amount * 10**18;
+        uint256 balance = deposited[msg.sender];
+        require(amount <= balance, "the amount more than bank of balance");
+        _;
     }
 
-    function deposit(uint amount) public {
-        amount = amount * 10 ** 18;
-        require(IERC20(token).transferFrom(msg.sender, address(this), amount), "transfer error");
+    function myBalance() public view returns (uint256 balance) {
+        balance = deposited[msg.sender] / (10**18);
+    }
+
+    function deposit(uint256 amount) public {
+        amount = amount * 10**18;
+        require(
+            IERC20(token).transferFrom(msg.sender, address(this), amount),
+            "transfer error"
+        );
         deposited[msg.sender] += amount;
+    }
+
+    function withdraw(uint256 amount) external requireBalance(amount) {
+        amount = amount * 10**18;
+        // require(IERC20(token).transfer(msg.sender, amount), "transfer error");
+        SafeERC20.safeTransfer(IERC20(token), msg.sender, amount);
+        deposited[msg.sender] -= amount;
+    }
+
+    function transfer(address to, uint256 amount)
+        public
+        requireBalance(amount)
+    {
+        amount = amount * 10**18;
+        deposited[msg.sender] -= amount;
+        deposited[to] += amount;
     }
 }
